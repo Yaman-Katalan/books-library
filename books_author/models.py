7191773869d70944a4714f84aa7267d97a73ea0c
Yaman-Katalan <23040456@student.ltuc.com>
2024-07-19 22:40:14 +0000

    abc

--- books_author/models.py
+++ books_author/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class Book(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
     rating = models.FloatField()
--- books_author/urls.py
+++ books_author/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
-from .views import HomePageView, BookListView
+from .views import HomePageView, BookListView, BookDetailsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('books/', BookListView.as_view(), name='book_list'),
+    path('books', BookListView.as_view(), name='book_list'),
+    path('books/<int:pk>/', BookDetailsView.as_view(), name='details'),
 ]
